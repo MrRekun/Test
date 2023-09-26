88a55e0ee78d18b784e056c9420baea56198cffa
MrRekun <145501113+MrRekun@users.noreply.github.com>
2023-09-26 17:26:52 +0100

    Update config.js

--- Configs/config.js
+++ Configs/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: '',
-    prefix: '',
-    devs: '',
-    embedcolor: '',
+    token: 'MTE1NTI2Mjk4NTI1ODkzNDM2Mw.Gerpye.ZJy-FMEqqRiy5Avk47Fps4zUm988Uo4EVigJ9o',
+    prefix: '.',
+    devs: '1061638397682274324',
+    embedcolor: '#109407',
     database: '',
     FACT_API: '',
     AME_API: ''
